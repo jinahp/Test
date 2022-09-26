54d3f9a64e95456a77bfe4c28da9fd2e0f7a29ce
jinah <monsatfri@gmail.com>
2022-09-26 22:41:39 +0900

    제어 흐름 이해하기

---
+++ Chapter4/src/loopexample/ForExample3.java
@@ -0,0 +1,16 @@
+package loopexample;
+
+public class ForExample3 {
+
+	public static void main(String[] args) {
+	
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Hello, World!");
+		}
+		System.out.println();
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("Hello, World!");
+			}
+		}
+}
