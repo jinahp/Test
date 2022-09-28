b102ff963900d17c8ebddfa9f0e247c1b7c2e588
jinah <monsatfri@gmail.com>
2022-09-28 22:02:00 +0900

    객체지향1

---
+++ Chapter5/src/classpart/Hello2.java
@@ -0,0 +1,10 @@
+package classpart;
+
+public class Hello2 {
+	// 소스파일의 이름은 public class이름과 일치
+	// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+	// 하나의 소스파일에는 하나의 public class만 허용
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
